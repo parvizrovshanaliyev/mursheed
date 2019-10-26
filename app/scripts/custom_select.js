a1c76f184b1b272fd93d9ccb42b5e6ed36bb14ea
parvizrovshanaliyev <parvizra@code.edu.az>
2019-10-26 20:37:09 +0400

    copy ok

---
+++ app/scripts/custom_select.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+
+    $('.select2-custom').select2({
+        // language: {
+        //     inputTooShort: function () {
+        //         return 'Zəhmət olmasa bir hərf daxil edin';
+        //     },
+        //     noResults: function () {
+        //         return "Nətice yoxdur";
+        //     },
+        //     searching: function () {
+        //         return "Axtarılır...";
+        //     }
+        // },
+        // placeholder: "Secin"
+    });
+    $('b[role="presentation"]').hide();
+    // $('.select2-selection__arrow').append('<i class="fa fa-angle-down pt-2"></i>');
+});
